c945d41e6911126521d3dd1c8aac3e7a0bff3934
Joshua Dixon <banewlf@hotmail.com>
2013-01-31 05:24:06 -0500

    Working random ladder generation

--- ChutesandLadders.cpp
+++ ChutesandLadders.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
+#include <time.h>
 using namespace std;
 
 /********************
@@ -12,7 +13,6 @@ struct gridspace
      std::string contents;
      gridspace *next_space; // Pointer to the next space on the board
      bool is_chute;
-     bool is_ladder;
      int board_position; // Numeric position in the board
      gridspace *next_rung; // Pointer to next rung of a chute/ladder
      gridspace();
@@ -43,19 +43,22 @@ void insert_player(gridspace *space, char symbol);
 gridspace *create_board(int rows, int cols);
 void delete_board(gridspace *head); // Recursive cleanup function
 void print_board(gridspace *head, int rows);
-
+gridspace *traverse(gridspace *start, int num);
+void generate_ladder(gridspace *start, int rows);
+void populate_board(gridspace *head, int rows, int cols);
 
 
 
 int main() {
-  const int row_size = 15;
-  const int col_size = 20;
+  const int row_size = 8;
+  const int col_size = 12;
   gridspace *head;
+  srand(time(NULL));
   head = create_board(row_size, col_size);
   player_data p1(head, '@'), p2(head, '&');
   print_board(head, row_size);
   system("PAUSE");
-  p1.do_move();
+  populate_board(head, row_size, col_size);
   print_board(head, row_size);
   delete_board(head);
   system("PAUSE");
@@ -73,7 +76,6 @@ gridspace::gridspace() // Default constructor
 {
      next_space = NULL;
      is_chute = 0;
-     is_ladder = 0;
      contents = "[   ]";
      board_position = 0;
      next_rung = NULL;
@@ -177,3 +179,63 @@ void print_board(gridspace *head, int rows)
            } while((temp->board_position % rows) != 0);     
      }
 }
+
+gridspace *traverse(gridspace *start, int num)
+{
+     int i;
+     for (i = 0; i < num; i++) {
+         if (start->next_space == NULL)
+            return start;
+         start = start->next_space;
+     }
+     return start;
+}
+
+void generate_ladder(gridspace *start, int rows)
+{
+     int i, middle;
+     gridspace *temp;
+     temp = start;   
+     
+     temp->is_chute = 1;
+     middle = (temp->contents.length()) / 2;
+     temp->contents[middle] = 'v';
+            
+     for (i = 0; i <= ((rand() % 3)+ 1); i++) { // creating between 3 and 5 rungs
+         temp->next_rung = traverse(temp, rows);
+         // Pointing next_rung to the space above temp
+         temp = temp->next_rung;
+         
+         if (temp == NULL) // shouldn't happen
+            return;
+         
+         temp->contents[middle] = 'v';
+         temp->is_chute = 1;
+      }
+}
+
+void populate_board(gridspace *head, int rows, int cols)
+{
+     int i, roll, num;
+     int board_size = (rows * cols);
+     gridspace *curr_space;
+     curr_space = head;
+     for (i = 1; (i) < (board_size - rows); i++) {
+         curr_space = curr_space->next_space;
+         
+         if(curr_space == NULL) // Shouldn't happen
+            return;
+         
+         if (curr_space->is_chute)  // If the space we're looking at is already
+            continue;              // a part of a chute or ladder
+         
+         roll = rand() % (rows * 3);
+         
+         
+         if (roll == 0)
+            generate_ladder(curr_space, rows);
+         
+     }
+}
+                
+         
